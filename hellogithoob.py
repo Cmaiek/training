633febe4bd0ee416c1c8610400dd953068a7da54
Maciej Kokot <maciej.kokot@gmail.com>
2018-08-29 21:48:51 +0200

    Zmieniłem funkcję, dodając zmienną z Franeczkiem uzależnioną od Ciampina

--- hellogithoob.py
+++ hellogithoob.py
@@ -1,6 +1,10 @@
 print("Hello GitHoob!")
 
 ciampin = "True"
+Francisz = 2
+
 if ciampin = "True"
-    print("Elo Ciampin")
-    
+    if Francisz < 3
+    print("Ciągle w żłobku")
+    else:
+        pass
