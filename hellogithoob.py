e3719ef0ebfd32a194abb99841ca189841aee1e5
Maciej Kokot <maciej.kokot@gmail.com>
2018-08-29 22:03:24 +0200

    Przewalczyłem poprawne zdefiniowanie zmiennej Ciampin i rozwiązałem błąd

--- hellogithoob.py
+++ hellogithoob.py
@@ -1,10 +1,10 @@
 print("Hello GitHoob!")
 
-ciampin = "True"
+ciampin = True
 Francisz = 2
 
-if ciampin = "True"
-    if Francisz < 3
-    print("Ciągle w żłobku")
+if ciampin:
+    if Francisz < 3:
+        print("Ciągle w żłobku")
     else:
         pass
